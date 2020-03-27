d69b07bc015788c785da8a9ed57da2017840d8e8
AlexHodovas <o.hodovas@gmail.com>
2020-03-27 12:55:05 +0200

    Updates

---
+++ precache-manifest.304b4afb29750a9fd6e61a8de6f9aa12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56c6cbc02d5ee709f9cf4c2b165730e",
+    "url": "/training_sagas/index.html"
+  },
+  {
+    "revision": "3362535cc657d85a3656",
+    "url": "/training_sagas/static/css/main.07b5549a.chunk.css"
+  },
+  {
+    "revision": "0b935cf1c0576b700155",
+    "url": "/training_sagas/static/js/2.d26a1881.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/training_sagas/static/js/2.d26a1881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3362535cc657d85a3656",
+    "url": "/training_sagas/static/js/main.a9614f42.chunk.js"
+  },
+  {
+    "revision": "37a7fd752f81c6eb0375",
+    "url": "/training_sagas/static/js/runtime-main.ccab705d.js"
+  }
+]);
