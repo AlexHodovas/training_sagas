3c8f8924f1fef8566ddc588744ae8c0abb771f86
prototype2010 <alex.zakorko2015@gmail.com>
2020-03-30 11:02:02 +0300

    working saga

--- src/redux/reducers/timerValueReducer.js
+++ src/redux/reducers/timerValueReducer.js
@@ -1,9 +1,20 @@
 import { ADD_ONE_TO_TIMER } from "../actions";
 
+const defaultState = {
+  LSItem : null,
+  time: 0
+};
+
 const timerValueReducer = (state = 0, action) => {
   switch (action.type) {
-    case ADD_ONE_TO_TIMER:
-      return state + action.value;
+    // case ADD_ONE_TO_TIMER:
+    //   return state + action.value;
+
+    case 'SET_STORAGE_ITEM':
+      return {
+        ...state,
+        LSItem: action.payload
+      };
 
     default:
       return state;
